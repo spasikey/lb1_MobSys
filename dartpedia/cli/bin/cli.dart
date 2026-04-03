f5816fe667fe5d54b97761474c2de3462b8f84b4
Alexandr Levtyakov <alevtyakov@mail.ru>
2026-04-03 16:33:14 +0300

    Выполнен блок: Define relationships with classes

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -4,7 +4,7 @@ import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) async { // main is now async and awaits the runner
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
