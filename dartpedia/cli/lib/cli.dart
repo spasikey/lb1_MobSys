295c8155b508c212e20d8d175ec1b9f4386a9d18
Alexandr Levtyakov <alevtyakov@mail.ru>
2026-04-03 17:39:51 +0300

    Выполнен блок: Add logging for debugging and monitoring

--- dartpedia/cli/lib/cli.dart
+++ dartpedia/cli/lib/cli.dart
@@ -1,3 +1,32 @@
-int calculate() {
-  return 6 * 7;
+ export 'src/commands/get_article.dart';
+ export 'src/commands/search.dart';
+ export 'src/logger.dart';
+
+import 'package:cli/cli.dart';
+import 'package:command_runner/command_runner.dart';
+
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+
+  app.run(arguments);
 }
